836bcc7e436a7ece6d8c989a842f0ba666fa2456
princenandoliya <princenandoliya@gmail.com>
2026-01-28 18:04:22 +0530

    Add files via upload

---
+++ ch-2-intro/2.let.js
@@ -0,0 +1,40 @@
+let Name = "hi"
+console.log(Name)
+
+function all(){
+
+    let Name = "i am"
+    console.log(Name)
+
+    if(true){
+        let ps = "prince nandoliya"
+        console.log(ps)
+        
+    }
+    if(true){
+        let pn = "full stack devloper"
+        console.log(pn)
+    }
+}
+
+all()
+
+//
+
+let a = 12;
+
+let b = 45;
+
+console.log(a+b)
+
+//re-intiliazation 
+
+
+let p = "my age is 18"
+// p="my age is not 18"
+
+console.log(p)
+
+//declare is not possible
+
+// let p=""
---
+++ ch-2-intro/3.const.js
@@ -0,0 +1,10 @@
+const p = 12
+// p = "13"
+//re-declare is not possible
+
+const n = 70
+//  n = "23"
+
+console.log(p + n)
+
+ 
