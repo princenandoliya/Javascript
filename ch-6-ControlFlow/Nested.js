1dc5fcb62374ca12a3118e557502e24efe814fe4
princenandoliya <princenandoliya@gmail.com>
2026-02-03 18:10:17 +0530

    Add files via upload

---
+++ ch-6-ControlFlow/Nested.js
@@ -0,0 +1,32 @@
+let Age = 18
+
+let voteid = false
+
+
+if(Age>=17){
+    if(voteid){
+        console.log("you can give vote");
+    }else{
+        console.log("you can apply voterID");
+    }
+}else{
+    console.log("you can't give vote")
+}
+
+
+// 2
+
+let totalAmount = 4000
+
+let isMember = false
+
+if(totalAmount >= 3000){
+    if(isMember){
+        console.log("you get 20% discount")
+    }else{
+        console.log("you get 10% discount")
+    }
+
+}else{
+    console.log("No discount available")
+}
