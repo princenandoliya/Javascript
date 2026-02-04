928df49189015b3530946ba1b5e75b4fa02eb034
princenandoliya <princenandoliya@gmail.com>
2026-02-04 11:48:53 +0530

    Add files via upload

---
+++ ch-6-ControlFlow/Ternary.js
@@ -0,0 +1,13 @@
+//1
+
+let Age = 18;
+
+let age = (Age >=18) ? "Eligible to vote" : "not Eligible to vote"
+console.log(age)
+
+//2
+
+let Car = 17;
+ 
+let car = (Car >= 18) ? "Eligible to drive a car" : "not Eligible to drive a car"
+console.log(car)
---
+++ ch-6-ControlFlow/switch–case.js
@@ -0,0 +1,55 @@
+//1
+
+let grade = "a";
+
+
+switch(grade){
+    case "a":
+    console.log("Excellent");
+    break;
+
+    case "b":
+    console.log("well Done");
+    break;
+
+    case "c":
+    console.log("Good");
+    break;
+
+    case "d":
+    console.log("pass");
+    break;
+
+    default:
+    console.log("fail");
+    
+}
+
+//2
+
+let brand ="Bmw"
+
+switch(brand){
+
+    case "Toyota":
+        console.log("Reliable nad fuel efficient");
+        break;
+
+
+        case "Bmw":
+        console.log("Luxury and performance");
+        break;
+
+
+        case "Tesla":
+        console.log("Electric and modern");
+        break;
+
+
+        case "Mahindra":
+            console.log("Strong build and made in india");
+            break;
+
+            default:
+                console.log("Brand not found")
+}
