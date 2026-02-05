be9453c6e2b848beabec88714e2024df59541c5b
princenandoliya <princenandoliya@gmail.com>
2026-02-05 11:28:49 +0530

    Update Operator.js

--- ch-4-Operator/Operator.js
+++ ch-4-Operator/Operator.js
@@ -1,25 +1,255 @@
-//Operator
+
+//operator
+
 
 //1.Arithmetic operator
 
-// let a = 10;
-// let b = 20;
+console.log("1.Arithmetic Operator ")
 
-// console.log("Addition =",a + b);
-// console.log("Substraction =",a - b);
-// console.log("Multiplication =",a * b);
-// console.log("Division =",a / b);
-// console.log("Modulo =",a % b);
-// console.log("Exponentiation ",a ** b);
+let a = 10
+let b = 20
 
-// console.log(" ") //for space
+console.log("Addition =", a + b)
+console.log("Substraction =", a - b)
+console.log("Multiplication =", a * b)
+console.log("Division =", a / b)
+console.log("Modulo =", a % b)
+console.log("Exponentiation =", a ** b)
 
-let a =40;
-let b =60;
+console.log(" ")//for space
 
-console.log("Addition",a + b);
-console.log("Substraction",a - b);
-console.log("Multiplication",a * b);
-console.log("Division",a / b);
-console.log("Modulo",a % b);
-console.log("Exponentiation", a *b);
+
+// let a = 50
+// let b = 100
+
+// console.log("Addition =", a + b)
+// console.log("Substraction =", a - b)
+// console.log("Multiplication =", a * b)
+// console.log("Division =", a / b)
+// console.log("Modulo =", a % b)
+// console.log("Exponentiation =", a ** b)
+
+//2.Assignment Operator
+
+console.log("2.Assignment Operator ")
+
+
+a = 2;
+b = 3;
+
+a += b;
+console.log("Add and assign A =", a);
+
+a -= b;
+console.log("Subtract and assign A =", a);
+
+a *= b;
+console.log("Multiply and assign A =", a);
+
+a /= b;
+console.log("Divide and assign A =", a);
+
+a %= b;
+console.log("Modulo and assign A =", a);
+
+console.log(" ")//for space
+
+
+// a = 20;
+// b = 30;
+
+// a += b;
+// console.log("Add and assign A =", a);
+
+// a -= b;
+// console.log("Subtract and assign A =", a);
+
+// a *= b;
+// console.log("Multiply and assign A =", a);
+
+// a /= b;
+// console.log("Divide and assign A =", a);
+
+// a %= b;
+// console.log("Modulo and assign A =", a);
+
+
+//3.Comparison operator
+
+console.log("3.Comparison operator")
+
+
+let A=10
+let B="20"
+
+console.log("Equal to =",A==B)
+
+console.log("Strict Equal to =",A===B)
+
+console.log("Not Equal to =", A != B);
+
+console.log("Not Equal to ,check data type =", A !== B);
+
+
+console.log("Less Than =",A<B)
+
+console.log("Greator Than =",A>B)
+
+console.log("Greator or Equal =",A>=B)
+
+console.log("Less or Equal =",A<=B)
+
+// let A=40
+// let B=30
+
+// console.log("Equal to =",A==B)
+
+// console.log("Strict Equal to =",A===B)
+
+// console.log("Not Equal to =", A != B);
+
+// console.log("Not Equal to ,check data type =", A !== B);
+
+
+// console.log("Less Than =",A<B)
+
+// console.log("Greator Than =",A>B)
+
+// console.log("Greator or Equal =",A>=B)
+
+// console.log("Less or Equal =",A<=B)
+
+console.log(" ")//for space
+
+
+
+
+//4.Logical operator
+
+console.log("4.logical operator ")
+
+//AND (&&)
+
+let marks1=95
+let marks2=80
+
+if(marks1 >=95 &&  marks2 >=80){
+    console.log("you have achieve grade A")
+}
+
+
+// let age = 20;
+// let hasID = true;
+
+// if (age >= 18 && hasID) {
+//     console.log("Entry allowed");
+// }
+
+
+
+//OR (||)
+
+let student1=90
+let student2=20
+
+if(student1>=40 || student2 >= 40){
+    console.log("You crack this exam")
+}
+
+
+
+
+// let hasPass = false;
+// let hasPermission = true;
+
+// if (hasPass || hasPermission) {
+//     console.log("You can enter the hall");
+// }
+
+
+
+//NOT (!)
+
+let Age=18;
+
+if(!(Age>=18)){
+    console.log("You cannot give vote")
+}else{
+    console.log("you can vote")
+}
+
+
+// let isLoggedIn = false;
+
+// if (!isLoggedIn) {
+//     console.log("Please login first");
+// }
+
+
+console.log(" ") //for space
+
+
+
+
+//5.Ternary Operator
+
+console.log("Ternary Operator")
+
+const Marks=40;
+const result=(Marks>=33)?"You Pass This Exam":"You Fail This Exam";
+
+console.log(result)
+
+
+// let number = 10;
+
+// let check = (number % 2 === 0) ? "Even Number" : "Odd Number";
+// console.log(check);
+
+console.log(" ") //for space
+
+
+//6.Type Operator
+
+console.log("Type Operator")
+
+let num=123
+let str="dharmik"
+let object={NAME:"Dharmik",Age:"18"}
+
+console.log(typeof num)
+console.log(typeof str)
+console.log(typeof object)
+
+
+// let arr = [10, 20, 30];
+// let func = function () {};
+
+// console.log(typeof arr);   
+// console.log(typeof func);
+
+console.log(" ") //for space
+
+
+
+//7.Increment / Decrement
+
+console.log("7.Increment And Decrement")
+
+//Pre Increment / Decrement
+
+let y = 10;
+console.log(++y); 
+
+//Post Increment / Decrement
+
+let x = 10;
+console.log(x++); 
+console.log(x);   
+
+
+
+
+
+
+
