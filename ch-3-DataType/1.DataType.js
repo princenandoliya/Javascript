365e48efe390471ecb65367111cc47309a0e75cb
princenandoliya <princenandoliya@gmail.com>
2026-02-06 15:13:09 +0530

    Add files via upload

---
+++ ch-3-DataType/1.DataType.js
@@ -0,0 +1,75 @@
+//Total 8  datatype
+//1.string
+//2.bigint
+//3.number
+//4.undefine
+//5.symbol
+//6.null
+//7.boolean
+//8.object
+
+
+//premitive datatype
+
+//1.string 
+
+let string = "prince"
+
+console.log(string)
+console.log(typeof string)
+
+//2.bigint
+
+let bigint = 121233n
+
+console.log(bigint)
+console.log(typeof bigint)
+
+//3.number
+
+let number = 1017
+
+console.log(number)
+console.log(typeof number)
+
+//4.undefine
+
+let undifine 
+
+console.log(typeof undifine)
+
+
+//5.symbol
+
+let c=Symbol(10)
+let a=Symbol(10)
+
+
+console.log(c==a)
+
+
+//6.null
+
+let n = null
+
+console.log(typeof n)
+
+//7.boolean
+
+let A = true
+
+console.log(A)
+console.log(typeof A)
+
+
+
+// non-premitive datatype
+
+//8.object
+
+let person = {Name: "prince" , Age: "18"}
+
+console.log(person)
+console.log(typeof person)
+
+
