3d9e3114763f3a17785c78a15d2d023c8092de31
princenandoliya <princenandoliya@gmail.com>
2026-02-11 12:28:54 +0530

    Add files via upload

---
+++ ch-11-string-method/1.stringmethod.js
@@ -0,0 +1,41 @@
+let all = "hi i am prince"
+
+//charAt()
+
+console.log("char At",all.charAt(9))
+
+//concat()
+
+console.log("concat method",all.concat(" i am web developer"))
+
+
+//endswith
+
+console.log("end with",all.endsWith("a"))
+console.log("end with",all.endsWith("e"))
+
+//includes
+
+console.log("include",all.includes("prince"))
+console.log("include" ,all.includes("bob"))
+
+
+//index ()
+
+console.log("indexof",all.indexOf("i"))
+
+//if not found
+console.log("indexof",all.indexOf("bob"))
+
+//last index
+console.log("last indexof",all.lastIndexOf("i"))
+
+
+//specific index
+console.log("specific index find",all.indexOf("i",2))
+
+//length returns
+
+console.log("length",all.length)
+
+
