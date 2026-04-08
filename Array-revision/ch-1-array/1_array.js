0d60d841b13404f5982ac12a98d43555e530a3e1
princenandoliya <princenandoliya@gmail.com>
2026-04-08 12:07:46 +0530

    Add files via upload

---
+++ Array-revision/ch-1-array/1_array.js
@@ -0,0 +1,7 @@
+const car = ["bmw","audi","i20","xuv 7000"]
+console.log("car",car)
+
+
+const bike = new Array ("bmw s1000rr","gt 650","R15","ducati")
+console.log(typeof bike)
+console.log("bike",bike)
---
+++ Array-revision/ch-1-array/2_array.js
@@ -0,0 +1,15 @@
+const car = ["bmw","audi","i20","Fortuner","Thar"]
+console.log("car",car)
+
+
+console.log("car",car[2])
+
+console.log("car",car[0])
+
+
+console.log("car",car[4])
+
+car[4] = "Skoda Rapid"
+console.log("car",car)
+
+console.log("car",car.length)
