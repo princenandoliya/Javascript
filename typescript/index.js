fa879998f950c3e637a394d1af8517a02a4b59fd
princenandoliya <princenandoliya@gmail.com>
2026-04-16 15:11:36 +0530

    Add files via upload

---
+++ typescript/index.js
@@ -0,0 +1,7 @@
+"use strict";
+let num = 20;
+let num1 = 40;
+function all(a, b) {
+    return a + b;
+}
+console.log(all(20, 60));
---
+++ typescript/index.ts
@@ -0,0 +1,7 @@
+ let num : number = 20;
+ let num1 : number = 40;
+ function all(a:number,b:number):number{
+    return a+b;
+ }
+
+ console.log(all(20,60))
