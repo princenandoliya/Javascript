7705ede2c9dfbac0addb5891b7a13111d88326f7
princenandoliya <princenandoliya@gmail.com>
2026-04-21 14:52:47 +0530

    Add files via upload

---
+++ Array-project/index.js
@@ -0,0 +1,79 @@
+
+let push = document.getElementById("push")
+let pop = document.getElementById("pop")
+let shift = document.getElementById("shift")
+let unshift = document.getElementById("unshift")
+let slice = document.getElementById("slice")
+let some = document.getElementById("some")
+let reverse = document.getElementById("reverse")
+let indexOf = document.getElementById("indexOf")
+let includes = document.getElementById("includes")
+let map = document.getElementById("map")
+let filter = document.getElementById("filter")
+let reduce = document.getElementById("reduce")
+let arr = [];
+
+let inp1 = document.getElementById("inp1")
+
+function star(){
+
+    let inp = document.getElementById("price").value
+
+    inp1.innerHTML = ""
+
+    for(let i=1; i <= inp;i++){
+        inp1.innerHTML += `
+           <input type="text" class="mb-2 form-control w-75" placeholder="Enter Element ${i}" id="el${i}">
+           `
+    }    
+}
+
+function loadArray() {
+    let count = document.getElementById("price").value; 
+
+    arr = [];
+
+    for (let i = 1; i <= count; i++) {
+        let val = document.getElementById("el" + i).value;
+
+        if (val !== "") {
+            arr.push(val); 
+        }
+    }
+
+    let out = document.getElementById("arrayoutput"); 
+    out.innerHTML = "Array : [ " + arr.join(", ") + " ]";
+}
+
+
+
+function getValues() {
+    let count = document.getElementById("price").value;
+    let arr = [];
+
+    for (let i = 1; i <= count; i++) {
+        let val = document.getElementById(`el${i}`).value;
+        if (val !== "") {
+            arr.push(val); 
+        }
+    }
+    return arr;
+}
+
+function pushMethod() {
+    let arr = getValues();
+    let newItem = prompt("Enter new item");
+    arr.push(newItem);
+
+    showOutput(arr);
+}
+
+function showOutput(data) {
+    let out = document.getElementById("output");
+
+    if (Array.isArray(data)) {
+        out.innerHTML = `output:- ${data.join(" , ")}`;
+    } else {
+        out.innerHTML = data;
+    }
+}
